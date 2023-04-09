a1c29c4e03b64dd34422909a783d670e0a0bfb8a
Santosh Kumar <skambharti563@gmail.com>
2023-04-10 00:40:30 +0530

    doen

--- private/view/includes/navigation.view.php
+++ private/view/includes/navigation.view.php
@@ -1,22 +1,47 @@
 <!-- Header start -->
 
-<header class="tws_header">
-    <nav class="tws_navigation">
-        <div class="tws_logo"><a href="#">TheWeb <span>Sources</span></a></div>
-        <div class="tws_nav">
-            <ul>
-                <li><a href="#">Home</a></li>
-                <li><a href="#">Downloads</a></li>
-                <li><a href="#">Tutorials</a></li>
-                <li><a href="#">Resources</a></li>
-            </ul>
-        </div>
-        <div class="tws_actions">
-            <ul>
-                <li><a href="#">Signin</a></li>
-            </ul>
-        </div>
-    </nav>
-</header>
+<!-- Header start -->
+<nav class="navbar shadow-sm sticky-top navbar-expand-lg tws_header py-3">
+  <div class="container-fluid">
+    <a class="navbar-brand text-dark" href="<?=BASE ?>">TheWeb</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav ms-5 mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link fw-bold px-lg-3 px-0 text-dark fs-6" href="<?=BASE ?>">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link fw-bold px-lg-3 px-0 text-dark fs-6" href="<?=BASE ?>source">Source</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link fw-bold px-lg-3 px-0 text-dark fs-6" href="<?=BASE ?>comming">Collage Projects</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link fw-bold px-lg-3 px-0 text-dark fs-6" href="<?=BASE ?>comming">Internship Projects</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link fw-bold px-lg-3 px-0 text-dark fs-6" href="<?=BASE ?>comming">Templates</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link fw-bold px-lg-3 px-0 text-dark fs-6" href="<?=BASE ?>comming">Courses</a>
+        </li>
+        </ul>
+
+        
+        <ul class="navbar-nav ms-auto">
+        <li class="nav-item">
+          
+             <a class="nav-link fw-bold button" href="'.BASE.'/signup">Signup</a>
+             
+        </li>
+        </ul>
+
+    </div>
+  </div>
+</nav>
+
+<!-- Header end -->
 
 <!-- Header end -->
