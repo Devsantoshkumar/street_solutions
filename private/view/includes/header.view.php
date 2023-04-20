2553ce90d50fffea1389afa8a25129f058c69d18
Santosh Kumar <skambharti563@gmail.com>
2023-04-21 02:45:30 +0530

    done

--- private/view/includes/header.view.php
+++ private/view/includes/header.view.php
@@ -1,3 +1,5 @@
+<!DOCTYPE html>
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -6,9 +8,9 @@
     <title>thewebsources</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" />
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Sharp:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
-    <link rel="stylesheet" href="<?= ASSETS ?>/css/bootstrap.css">
-    <link rel="stylesheet" href="<?= ASSETS ?>/css/global.css">
-    <link rel="stylesheet" href="<?= ASSETS ?>/css/style.css">
-    <link rel="stylesheet" href="<?= ASSETS ?>/css/media-query.css">
+    <link rel="stylesheet" href="<?= ASSETS ?>css/bootstrap.css">
+    <link rel="stylesheet" href="<?= ASSETS ?>css/global.css">
+    <link rel="stylesheet" href="<?= ASSETS ?>css/style.css">
+    <link rel="stylesheet" href="<?= ASSETS ?>css/media-query.css">
 </head>
 <body>
