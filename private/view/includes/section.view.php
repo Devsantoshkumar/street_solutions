5b28dc10bb8c3238204abcef5f791c4f8f6aae9a
nitesh kumar jha <nitesh930743@gmail.com>
2023-05-10 09:10:21 +0530

    done

--- private/view/includes/section.view.php
+++ private/view/includes/section.view.php
@@ -4,7 +4,7 @@
     <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
     <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
   </div>
-  <div class="carousel-inner" style="height: 400px">
+  <div class="carousel-inner" style="height: 600px">
     <div class="carousel-item active">
       <img src="<?=ASSETS ?>images/img1.jpg" class="d-block w-100" alt="...">
     </div>
