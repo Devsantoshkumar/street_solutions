ed90f2ec1314d38604ac24e377ac8d341a0ff603
nitesh kumar jha <nitesh930743@gmail.com>
2023-05-10 09:05:16 +0530

    adding caursel

--- private/view/includes/navigation.view.php
+++ private/view/includes/navigation.view.php
@@ -1,2 +1,24 @@
-
-Hello
+<nav class="navbar navbar-expand-lg" id="navigation">
+  <div class="container-fluid">
+    <a class="navbar-brand ms-sm-5" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active ms-sm-5" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link ms-sm-5" href="#">Features</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link ms-sm-5" href="#">Pricing</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link ms-sm-5">Disabled</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
---
+++ private/view/includes/section2.view.php
@@ -0,0 +1,35 @@
+<div id="box">
+    <br>
+   <center><h3 class="ms-sm-5 mb-sm-2">Request a quote</h3></center> 
+   <br><br>
+    <div class="row">
+        <div class="col-4 ms-sm-3">
+          <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="your name" >
+
+        </div>
+        <div class="col-3">
+           <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="your email">
+
+        </div>
+        <div class="col-4 ms-sm-2">
+          <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="service">
+
+        </div>
+    </div>
+    <br><br>
+    <div class="row">
+        <div class="col-4 ms-sm-3">
+          <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="your name" >
+
+        </div>
+        <div class="col-3">
+           <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="your email">
+
+        </div>
+        <div class="col-4 ms-sm-2">
+          <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="service">
+
+        </div>
+    </div>
+
+</div>
