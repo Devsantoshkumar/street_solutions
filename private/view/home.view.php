07a626a50ee08450d35a4a4259ba5d19cc1510fa
nitesh kumar jha <nitesh930743@gmail.com>
2023-05-11 09:39:18 +0530

    done

--- private/view/home.view.php
+++ private/view/home.view.php
@@ -15,8 +15,43 @@
           <div class="container">
             <div class="row">
               <div class="col">
-                   Hello
-              </div>
+                <div class="card border-0 p-4 homelogin">
+                   <form action="">
+                    <div class="row">
+                       <h4 class="text-center text-light py-2">Login</h4>
+                    </div>
+                   <div class="row">
+        <div class="col">
+           <input type="email" name="email" value="<?=get_val("email")?>" class="form-control inputFields" id="exampleFormControlInput1" placeholder="your email">
+          </div>
+    </div>
+    <br><br>
+
+
+    <div class="row">
+        <div class="col">
+          <input type="password" name="password" class="form-control inputFields" id="exampleFormControlInput1" placeholder="Password" >
+          <small class="text-danger"><?=isset($errors['password']) ? $errors['password'] : "" ?></small>
+        </div>
+    </div>
+    <br><br>
+
+    <div class="row">
+        <div class="col">
+          <small>I have already an account. <a href="<?=BASE ?>signup" class="badge bg-primary">Signup</a></small>
+        </div>
+    </div>
+    <br><br>
+
+    <div class="row">
+        <div class="col-3 px-5">
+            <button class="btn btn-sm btn-primary" type="submit">Signup</button>
+        </div>
+    </div>
+    </form>
+     
+                </div>
+             </div>
             </div>
           </div>
       </div>
